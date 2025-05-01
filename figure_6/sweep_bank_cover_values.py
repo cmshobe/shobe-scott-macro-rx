a557442c4e2dd1a931a85111d12272ec7ebc50bd
cmshobe <shobe.charlie@gmail.com>
2025-05-01 11:15:27 -0600

    edit core fn name in fig 6

--- figure_6/sweep_bank_cover_values.py
+++ figure_6/sweep_bank_cover_values.py
@@ -11,7 +11,7 @@ sweep ONE AXIS of the parameter space in a parallelized way using multiprocessin
 from multiprocessing import Pool
 from functools import partial
 import numpy as np
-from roughness_function import wood
+from roughness_function import channel_evolution
 import time
 import os
 
